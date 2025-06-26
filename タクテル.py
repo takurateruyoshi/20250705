5b6aee91ddfabbe4b4aef6ae7f7d09de30584314
sonobe <s2522023@stu.musashino-u.ac.jp>
2025-06-26 15:33:54 +0900

    jfieja;ioej

--- タクテル.py
+++ タクテル.py
@@ -1,1 +1,11 @@
 import py5
+
+def setup():
+    py5.size(400, 400)
+    py5.background(255)
+    py5.fill(0)
+    py5.text_size(32)
+    py5.text_align(py5.CENTER, py5.CENTER)
+    py5.text("Hello, Py5!", 200, 200)
+
+py5.run_sketch()
